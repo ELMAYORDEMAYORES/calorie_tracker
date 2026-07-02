489d13fe1b40d58aaf51e8a645cafc1d13675f75
ELMAYORDEMAYORES <153030832+ELMAYORDEMAYORES@users.noreply.github.com>
2026-07-02 14:41:30 -0400

    commit final

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer, useEffect, useMemo  } from 'react'
 import Form from "./components/Form"
 import { activityReducer, initialState } from './reducers/activity-reducer'
 import ActivityList from './components/ActivityList'
+import CalorieTracker from './components/CalorieTracker'
 function App() {
 
   const [state, dispatch] = useReducer(activityReducer, initialState)
@@ -15,7 +16,7 @@ function App() {
     return (
       <>
         <header className="bg-lime-600 py-3">
-          <div className="max-w-4xl mx-auto flex justify-between">
+          <div className="max-w-4xl mx-auto flex justify-between items-center">
             <h1 className="text-center text-lg font-bold text-white uppercase">
               Contador de Calorias
             </h1>
@@ -30,6 +31,14 @@ function App() {
             <Form 
               dispatch={dispatch}
               state={state}
+            />
+          </div>
+        </section>
+
+        <section className="bg-gray-800 py-10">
+          <div className="max-w-4xl mx-auto">
+            <CalorieTracker 
+              activities={state.activities}
             />
           </div>
         </section>
